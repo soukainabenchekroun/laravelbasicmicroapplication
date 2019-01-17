2ea35cca190f065eab40be33029389566951e180
Soukaina benchekroun <Soukainabenchekroun3@gmail.com>
2019-01-17 11:50:46 +0100

    Models

--- server/app/Company.php
+++ server/app/Company.php
@@ -1,10 +1,16 @@
 <?php
 
 namespace App;
+use App\Contact;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Company extends Model
 {
-    //
+    protected $fillable = ['name', 'address'];
+
+    public function contact()
+    {
+        return $this->hasOne(Contact::class);
+    }
 }
--- server/app/Contact.php
+++ server/app/Contact.php
@@ -1,10 +1,26 @@
 <?php
 
 namespace App;
+use App\Company;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Contact extends Model
 {
-    //
+    protected $fillable = [
+        'birth_date',
+        'first_name',
+        'last_name',
+        'function',
+        'phone_number',
+        'email_address',
+        'id_company',
+        'city',
+    ];
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+
 }
